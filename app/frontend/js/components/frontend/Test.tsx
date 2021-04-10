0ef2f8622d490d63d4ccc73033cf974ae08e2360
teraoka <temple.hill@icloud.com>
2021-04-10 17:13:30 +0900

    react bootstrap

--- app/frontend/js/components/frontend/Test.tsx
+++ app/frontend/js/components/frontend/Test.tsx
@@ -1,14 +1,29 @@
 import * as React from 'react';
+import Modal from '../shared/Modal';
 
 type Props = {
 }
 
 const Test: React.FC<Props> = ({
 }) => {
+  const [isModalOpen, setIsModalOpen] = React.useState<boolean>(false);
 
   return (
     <div>
-      Reactテスト
+      <button
+        onClick={() => {
+          setIsModalOpen(true);
+        }}
+      >
+        モーダル表示
+      </button>
+
+      <Modal
+        isOpen={isModalOpen}
+        setIsOpen={setIsModalOpen}
+      >
+        <p>てすと</p>
+      </Modal>
     </div>
   );
 };
